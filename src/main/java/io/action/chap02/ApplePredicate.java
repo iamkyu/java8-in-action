aef86a9a01955de3de92f2181f13074cffc7ee03
iamkyu <kyujnam@gmail.com>
2017-11-24 11:51:52 +0900

    기존 학습 코드 제거

--- src/main/java/io/action/chap02/ApplePredicate.java
+++
@@ -1,9 +0,0 @@
-package io.action.chap02;
-
-/**
- * @author Kj Nam
- * @since 2016-09-17
- */
-public interface ApplePredicate {
-    boolean test(Apple apple);
-}
--- src/main/java/io/action/chap02/Predicate.java
+++
@@ -1,9 +0,0 @@
-package io.action.chap02;
-
-/**
- * @author Kj Nam
- * @since 2016-09-17
- */
-public interface Predicate<T> {
-    boolean test(T t);
-}
